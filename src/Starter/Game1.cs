c527076e3599a0e8de061e3e9fa5c50030d101f7
Arissaman Nanajai <Arissaman.apple@gmail.com>
2026-07-25 14:03:33 +0700

    Update assembly information, refactor Game1 class, and implement card and event handling logic

--- src/Starter/Game1.cs
+++ src/Starter/Game1.cs
@@ -22,6 +22,26 @@ public class CardData
         Value = value;
         Type = type;
     }
+
+    public enum CardType
+    {
+        Attack,
+        Defense,
+        Heal
+    }
+
+    // Fixed: Returns a list of cards instead of multiple unreachable returns
+    public static List<CardData> MakeStartingDeck()
+    {
+        List<CardData> deck = new List<CardData>();
+        deck.Add(new CardData("Strike", 10, 6, "Attack"));
+        deck.Add(new CardData("Strike", 10, 6, "Attack"));
+        deck.Add(new CardData("Defend", 5, 4, "Defense"));
+        deck.Add(new CardData("Defend", 5, 4, "Defense"));
+        deck.Add(new CardData("Heal", 8, 5, "Heal"));
+        deck.Add(new CardData("Heal", 8, 5, "Heal"));
+        return deck;
+    }
 }
 
 public class RoomData 
@@ -31,6 +51,36 @@ public class RoomData
     public RoomData(string roomType) 
     {
         RoomType = roomType;
+    }
+}
+
+public enum EventType
+{
+    Cursed,
+    Blessed
+}
+
+public class CardRectangle
+{
+    public CardData Data { get; set; }
+    public Rectangle Bounds { get; set; }
+    public bool IsSelected { get; set; }
+
+    public CardRectangle(CardData data, Rectangle bounds)
+    {
+        Data = data;
+        Bounds = bounds;
+        IsSelected = false;
+    }
+
+    public void Draw(SpriteBatch spriteBatch, Texture2D whitePixel)
+    {
+        Color cardColor = Color.White;
+        if (Data.Type == "Attack") cardColor = Color.LightCoral;
+        else if (Data.Type == "Defense") cardColor = Color.LightBlue;
+        else if (Data.Type == "Heal") cardColor = Color.LightGreen;
+
+        spriteBatch.Draw(whitePixel, Bounds, cardColor);
     }
 }
 
@@ -100,6 +150,59 @@ public class RoomManager
         recentEncounters = 0;
         recentEvents = 0;
     }
+
+    public void EventRoom(ref int hp, ref int sanity, EventType eventType)
+    {
+        int roll = rand.Next(0, 100);
+        if (roll < 50)
+        {
+            hp -= 5;
+            sanity -= 10;
+        }
+        else
+        {
+            // Implement the effects of a blessed event here
+            hp += 10;
+            sanity += 20;
+        }
+    }
+}
+
+// Fixed: Methods are now properly separated outside of each other
+public class MonsterHandler
+{
+    public int EnemyHp = 30;
+    public int BossHp = 120;
+    public int EnemyActionCount = 4;
+    public int BossActionCount = 3;
+
+    public void MonsterAction(ref int playerHp)
+    {
+        int enemyDamage = 3;
+        if (EnemyActionCount == 0)
+        {
+            playerHp -= enemyDamage;
+            EnemyActionCount = 4;
+        }
+        else
+        {
+            EnemyActionCount--;
+        }
+    }
+
+    public void BossAction(ref int playerHp)
+    {
+        int bossDamage = 5;
+        if (BossActionCount == 0)
+        {
+            playerHp -= bossDamage;
+            BossActionCount = 3;
+        }
+        else
+        {
+            BossActionCount--;
+        }
+    }
 }
 
 public class Game1 : Game
@@ -108,21 +211,21 @@ public class Game1 : Game
     private SpriteBatch _spriteBatch;
     private Texture2D _whitePixel;
     
-    int hp =15;
+    int hp = 15;
     int sanity = 100;
     int holdCard = 6;
-    int CardMax = 12;
-    int Enermyhp = 30;
-    int Bosshp = 120;
-    int ClickLimit = 10;
+    int ClickLimit = 6;
     
     bool isEnermyDead = false;
-    bool isBossActive = false; // Tracks if we are fighting the boss
+    bool isBossActive = false; 
+    bool Skipturn = false; // skip turn when player has no Sanity left to play
 
     private MouseState previousMouse;
     private MouseState currentMouse;
     
     private RoomManager roomManager = new RoomManager();
+    private MonsterHandler monsterHandler = new MonsterHandler();
+    private List<CardData> handCards = new List<CardData>();
 
     public Game1()
     {
@@ -136,6 +239,8 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
+        handCards = CardData.MakeStartingDeck();
+        ClickLimit = 6;
         base.Initialize();
     }
 
@@ -155,34 +260,41 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
         
-        // Left Click Logic
         if (currentMouse.LeftButton == ButtonState.Pressed && previousMouse.LeftButton == ButtonState.Released)
         {
             Point mousePos = new Point(currentMouse.X, currentMouse.Y);
+            Rectangle skipRect = new Rectangle(1000, 600, 80, 40);
 
-            // 1. If enemy/boss is alive, click to damage them
-            if (!isEnermyDead && !isBossActive && ClickLimit > 0)
+            if (!isEnermyDead)
             {
-                if (isBossActive)
+                if (Skipturn)
                 {
-                    Bosshp -= 6; // Deal damage to boss
-                    ClickLimit--; // Decrease click limit for boss fight
-                    if (Bosshp <= 0)
+                    AdvanceTurn();
+                    Skipturn = false;
+                }
+                else if (skipRect.Contains(mousePos))
+                {
+                    Skipturn = true;
+                }
+                else if (handCards.Count > 0 && ClickLimit > 0)
+                {
+                    for (int i = 0; i < handCards.Count && i < holdCard; i++)
                     {
-                        isEnermyDead = true; // Win condition reached
+                        int xPos = 100 + (i * 100);
+                        Rectangle cardRect = new Rectangle(xPos, 600, 80, 120);
+
+                        if (cardRect.Contains(mousePos))
+                        {
+                            UseCard(i);
+                            break;
+                        }
                     }
                 }
                 else
                 {
-                    Enermyhp -= 6; // Deal damage to standard enemy
-                    ClickLimit--; // Decrease click limit for normal fight
-                    if (Enermyhp <= 0)
-                    {
-                        isEnermyDead = true;
-                    }
+                    Skipturn = true;
                 }
             }
-            // 2. If enemy is dead, check clicks on the 3 Brown Doors
             else
             {
                 for (int i = 0; i < 3; i++)
@@ -192,23 +304,23 @@ public class Game1 : Game
 
                     if (doorRect.Contains(mousePos))
                     {
-                        // Simulate choosing a door
                         string chosenType = roomManager.GetNextBrownDoorType();
                         roomManager.RegisterRoomCompletion(chosenType);
 
-                        // Check if boss room is ready after completion
                         if (roomManager.IsBossRoomReady())
                         {
                             isBossActive = true;
-                            Bosshp = 120; // Reset boss HP
+                            monsterHandler.BossHp = 120; 
                         }
                         else
                         {
-                            Enermyhp = 30; // Reset normal enemy HP
+                            monsterHandler.EnemyHp = 30; 
                             isBossActive = false;
                         }
 
-                        isEnermyDead = false; // Reset encounter state to fight again
+                        isEnermyDead = false;
+                        ClickLimit = 6;
+                        handCards = CardData.MakeStartingDeck();
                         break;
                     }
                 }
@@ -240,11 +352,22 @@ public class Game1 : Game
         {
             for (int i = 0; i < 3; i++)
             {
-                int xPos = 300 + (i * 200);
+                int xPos = 300 + (i * 200); 
                 _spriteBatch.Draw(_whitePixel, new Rectangle(xPos, 230, 128, 128), Color.Black);
             }
         }
 
+        // Card Display
+        for (int i = 0; i < handCards.Count && i < holdCard; i++)
+        {
+            int xPos = 100 + (i * 100);
+            CardData card = handCards[i];
+            Color cardColor = card.Type == "Attack" ? Color.LightCoral : card.Type == "Defense" ? Color.LightBlue : Color.LightGreen;
+            _spriteBatch.Draw(_whitePixel, new Rectangle(xPos, 600, 80, 120), cardColor);
+        }
+        // SkipButton Display
+        Color skipButtonColor = Skipturn ? Color.Goldenrod : Color.Gray;
+        _spriteBatch.Draw(_whitePixel, new Rectangle(1000, 600, 80, 40), skipButtonColor);
         // Player UI Status Bars
         _spriteBatch.Draw(_whitePixel, new Rectangle(64, 64, 256, 32), Color.Green);
         _spriteBatch.Draw(_whitePixel, new Rectangle(64, 96, 192, 24), Color.LightBlue);
@@ -252,5 +375,61 @@ public class Game1 : Game
         _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    private void UseCard(int cardIndex)
+    {
+        if (cardIndex < 0 || cardIndex >= handCards.Count)
+            return;
+
+        CardData card = handCards[cardIndex];
+        handCards.RemoveAt(cardIndex);
+        ClickLimit--;
+
+        if (card.Type == "Attack")
+        {
+            if (isBossActive)
+            {
+                monsterHandler.BossHp -= card.Value;
+                if (monsterHandler.BossHp <= 0)
+                    isEnermyDead = true;
+            }
+            else
+            {
+                monsterHandler.EnemyHp -= card.Value;
+                if (monsterHandler.EnemyHp <= 0)
+                    isEnermyDead = true;
+            }
+        }
+        else if (card.Type == "Defense")
+        {
+            hp += card.Value;
+            sanity += 2;
+        }
+        else if (card.Type == "Heal")
+        {
+            hp += card.Value;
+            sanity += 5;
+        }
+
+        if (ClickLimit <= 0 || handCards.Count == 0)
+        {
+            Skipturn = true;
+        }
+    }
+
+    private void AdvanceTurn()
+    {
+        if (isBossActive)
+        {
+            monsterHandler.BossAction(ref hp);
+        }
+        else
+        {
+            monsterHandler.MonsterAction(ref hp);
+        }
+        sanity += 10; //Regain some sanity each turn
+        ClickLimit = 6;
+        handCards = CardData.MakeStartingDeck();
     }
 }
--- src/Starter/obj/Debug/net9.0/Starter.AssemblyInfo.cs
+++ src/Starter/obj/Debug/net9.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce4060d5590a08eea208a78a25b690c44f655d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe547b14846f85f8c26213e9817e167790bcb0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
