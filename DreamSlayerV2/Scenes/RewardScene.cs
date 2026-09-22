352b071d875b15c5c8330d7559a915b35b6b4a92
Arissaman Nanajai <Arissaman.apple@gmail.com>
2026-09-22 16:31:52 +0700

    Update workspace state and modify scene dialogues for improved narrative flow

--- DreamSlayerV2/Scenes/RewardScene.cs
+++ DreamSlayerV2/Scenes/RewardScene.cs
@@ -29,6 +29,7 @@ namespace DreamSlayerV2.Scenes
                 _soulReward = _rand.Next(20, 31);
                 _isEliteOrBoss = false;
                 _enemyTypeLabel = "Enemy";
+                // Test
             }
             else if (rewardTier == 1) // Elite (50-75 Soul Coins + Relic chance for full version)
             {
